88ca3c8ad580d47cdb61257f7ff0f71a9450ed95
taisuke0804 <m83.matsuura.taisuke@gmail.com>
2025-08-17 22:58:32 +0900

    feat: AdminLoginController作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\Admin\AdminLoginController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
@@ -13,6 +14,18 @@ Route::get('dashboard', function () {
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
+
+// ------------------------------------------------------------------------------------------------
+/**
+ * 管理者認証関連のルート
+ * 管理者専用のログイン処理を設定
+ */
+
+Route::prefix('admin')->name('admin')->group(function () {
+    Route::controller(AdminLoginController::class)->group(function () {
+        Route::get('login', 'showLoginForm')->name('login');
+    });
+});
 
 // Route::get('/test', function () {
 //     return Inertia::render('Test/Test');
