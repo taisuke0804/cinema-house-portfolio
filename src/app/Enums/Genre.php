4e9821287e4f37e7d3f75556d7ec693d77f11fa2
taisuke0804 <m83.matsuura.taisuke@gmail.com>
2025-09-15 15:46:34 +0900

    feat: 映画ジャンルのEnumを実装、ラベル・選択肢生成メソッドを追加

---
+++ src/app/Enums/Genre.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Enums;
+
+enum Genre: int
+{
+    case Action = 1;
+    case Comedy = 2;
+    case Drama = 3;
+    case Horror = 4;
+    case Romance = 5;
+    case Other = 99;
+
+    /**
+     * 映画ジャンルのラベルを取得
+     */
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::Action => 'アクション',
+            self::Comedy => 'コメディ',
+            self::Drama => 'ドラマ',
+            self::Horror => 'ホラー',
+            self::Romance => 'ロマンス',
+            self::Other => 'その他',
+        };
+    }
+
+    /**
+     * 全ての値を配列で取得
+     */
+    public static function values(): array
+    {
+        return array_map(fn(self $genre) => $genre->value, self::cases());
+    }
+
+    /**
+     * セレクトボックスなどで利用可能な形式の配列を返す
+     */
+    public static function options(): array
+    {
+        return array_map(fn(self $genre) => [
+            'value' => $genre->value,
+            'label' => $genre->getLabel(),
+        ], self::cases());
+    }
+}
