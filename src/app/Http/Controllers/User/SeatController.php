b388ac2f064e6daf02f38d72ca69e375478176a8
taisuke0804 <m83.matsuura.taisuke@gmail.com>
2025-09-29 17:25:57 +0900

    feat: 座席予約用リクエストクラスを実装

--- src/app/Http/Controllers/User/SeatController.php
+++ src/app/Http/Controllers/User/SeatController.php
@@ -4,14 +4,19 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\User\ReserveSeatRequest;
+use Illuminate\Support\Facades\Auth;
 
 class SeatController extends Controller
 {
     /**
      * 座席を予約する処理
      */
-    public function reserve(Request $request)
+    public function reserve(ReserveSeatRequest $request)
     {
-        dd($request);
+        $validated = $request->validated();
+        $validated['user_id'] = Auth::id('web');
+
+        dd($validated);
     }
 }
