82d24467cd868ce821a5d4e0d0cb898d7c6609e9
taisuke0804 <m83.matsuura.taisuke@gmail.com>
2025-10-06 20:06:36 +0900

    chore: screenings:delete-past コマンドを毎日3時に実行するスケジュールを登録

--- src/routes/console.php
+++ src/routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('screenings:delete-past')
+    ->dailyAt('03:00')
+    ->timezone('Asia/Tokyo')
+    ->withoutOverlapping() // タスク多重起動の防止
+    ->evenInMaintenanceMode(); // メンテ中も実行
