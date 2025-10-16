8469121c05202c2ae0bd42c01d7c9310b7b093a3
taisuke0804 <m83.matsuura.taisuke@gmail.com>
2025-10-16 21:20:40 +0900

    feat: Admin側ユーザー削除処理 ルート・メソッドを定義

--- src/routes/admin.php
+++ src/routes/admin.php
@@ -47,6 +47,7 @@ Route::middleware(['web', 'admin'])->group(function () {
             Route::get('users/create', 'create')->name('users.create');
             Route::post('users', 'store')->name('users.store');
             Route::get('users/{id}', 'show')->name('users.show');
+            Route::delete('users/{id}', 'destroy')->name('users.destroy');
         });
     });
 
